f7468e046ece0e5405075cd121308f5163288bed
Akhil Bidhuri <akhilbidhuri@gmail.com>
2024-04-20 00:25:33 +0530

    add basic palyer capabilities, anims

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,6 +12,23 @@ public class Player : MonoBehaviour
     private int facingDirecction = 1;
     private bool facingRight = true;
 
+    [Header("Dash Info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashCooldown;
+    private float dashTime;
+    private float dashCooldownTimer;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+    [Header("Attack Info")]
+    private bool isAttacking;
+    private int comboCounter;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,34 +39,74 @@ public class Player : MonoBehaviour
     {
         Movement();
         CheckInput();
+
+        CollisionChecks();
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+       
         FlipController();
         AnimatorController();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        { 
+            isAttacking = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
         }
     }
 
     private void Movement()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(xInput * dashSpeed, 0);
+        }
+        else
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
     private void AnimatorController()
     {
         bool isMoving = rb.velocity.x != 0;
+        anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
     }
 
     private void Flip() {
@@ -64,5 +121,15 @@ public class Player : MonoBehaviour
             Flip();
         else if (rb.velocity.x < 0 && facingRight)
             Flip();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y -  groundCheckDistance));
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
     }
 }
